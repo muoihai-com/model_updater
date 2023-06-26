959a15a09c3c241e113c6b99100e847f543996f7
Hung Kieu Quoc (hungkq1724) <kieu.quoc.hung@sun-asterisk.com>
2023-06-26 13:34:14 +0700

    0.1.0

--- lib/model_updater/client.rb
+++ lib/model_updater/client.rb
@@ -2,11 +2,20 @@ module ModelUpdater
   class Client
     class << self
       def models
-        ::ApplicationRecord.descendants.map(&:name)
+        valid_models = ModelUpdater.configuration.valid_models
+        if valid_models.blank?
+          ::ApplicationRecord.descendants.map(&:name)
+        else
+          ::ApplicationRecord.descendants.map(&:name) & valid_models
+        end
       end
 
       def model name
         klass = Object.const_get(name)
+        valid_models = ModelUpdater.configuration.valid_models
+        if valid_models.present? && !ModelUpdater.configuration.valid_models.include?(klass.name)
+          raise
+        end
 
         ModelUpdater::Proxy.new(klass)
       end
